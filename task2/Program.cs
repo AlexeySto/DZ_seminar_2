b4463303c29f125aa87205dd5d8afb36a77e497c
Алексей <stolyarov1986@inbox.ru>
2023-07-09 12:29:31 +0300

    added task2

---
+++ task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int ThirdDigit(int num)            // Находит цифру третьего порядка
+{
+    return (num - (num / 1000) * 1000 - num % 100) / 100; 
+}
+
+int MinusDelet(int num1)            // Если число отрицательное, то переводим в положительное
+{
+    if (num1 < 0)
+    {
+        num1 = num1 * -1;
+    }
+    return num1;
+}
+
+if (MinusDelet(number) > 99)
+{
+    Console.WriteLine(number + " -> " + ThirdDigit(MinusDelet(number)));
+}
+else
+{
+    Console.WriteLine($"{number} -> третьей цифры нет.");
+}
